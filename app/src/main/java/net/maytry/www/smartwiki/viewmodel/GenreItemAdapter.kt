ca85276ee3e7f5343f47f82dc31217ec57806926
slont <renma.da@gmail.com>
2016-09-07 02:04:54 +0900

    Adapter追加・修正

--- app/src/main/java/net/maytry/www/smartwiki/viewmodel/GenreItemAdapter.kt
+++ app/src/main/java/net/maytry/www/smartwiki/viewmodel/GenreItemAdapter.kt
@@ -6,47 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ToggleButton
 import net.maytry.www.smartwiki.R
-import net.maytry.www.smartwiki.databinding.GenreListItemBinding
-import net.maytry.www.smartwiki.model.Genre
+import net.maytry.www.smartwiki.databinding.GenreItemListItemBinding
+import net.maytry.www.smartwiki.model.GenreItem
 
 /**
- * ジャンル一覧のアダプタ
+ * 一覧のアダプタ
  *
- * Created by slont on 8/7/16.
+ * Created by slont on 8/16/16.
  */
-class GenreAdapter(context: Context, textViewResourceId: Int, items: List<Genre>) :
-        ArrayAdapter<Genre>(context, textViewResourceId, items) {
+class GenreItemAdapter(context: Context, textViewResourceId: Int, items: List<GenreItem>) :
+        ArrayAdapter<GenreItem>(context, textViewResourceId, items) {
 
     val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         val view: View
-        val binding: GenreListItemBinding
+        val binding: GenreItemListItemBinding
         if (null == convertView) {
-            binding = DataBindingUtil.inflate(inflater, R.layout.genre_list_item, parent, false)
+            binding = DataBindingUtil.inflate(inflater, R.layout.genre_item_list_item, parent, false)
             view = binding.root
             view.tag = binding
         } else {
-            binding = convertView.tag as GenreListItemBinding
+            binding = convertView.tag as GenreItemListItemBinding
             view = convertView
         }
-        binding.genre = getItem(position)
+        binding.item = getItem(position)
         binding.onClickFavoriteButtonListener = OnClickFavoriteButtonListener(getItem(position))
         return view
     }
 
     /**
-     * change genre favorite state
      *
-     * @property genre
+     * @property content
      */
-    class OnClickFavoriteButtonListener(genre: Genre) : View.OnClickListener {
-        val genre: Genre = genre
+    class OnClickFavoriteButtonListener(content: GenreItem) : View.OnClickListener {
+        val content: GenreItem = content
 
         override fun onClick(v: View?) {
-            genre.favorite = (v as ToggleButton).isChecked
+//            genre.favorite = (v as ToggleButton).isChecked
         }
     }
 }
